34d19c71f3409020fa00c0be41455dc446220757
Moniruggaman <moniruggaman@gmai.com>
2021-04-05 00:50:16 +0800

    ema-john has done

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -5,22 +5,27 @@ import './Product.css';
 
 
 const Product = (props) => {
-    console.log(props);
+    // console.log(props);
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className="product">
             <div>
-                <img src={img} alt=""/>
+                <img src={img} alt="" />
             </div>
             <div>
                 <h3 className="product-name">{name}</h3>
-                <br/>
+                <br />
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart}/> add to cart</button>
+                <button
+                    className="main-button"
+                    // using arrow function onclick handler to prevent auto execute the product.
+                    onClick={() => props.handleAddProduct(props.product)}
+                        ><FontAwesomeIcon icon={faShoppingCart} /> add to cart
+                </button>
             </div>
-            
+
         </div>
     );
 };
